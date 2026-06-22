7c59c5506a2ff54868816919eb3243b5472ef035
WeiZe-Lu <854316318@qq.com>
2026-06-22 22:17:06 +0800

    6.22

--- Core/Hardware/Motor.c
+++ Core/Hardware/Motor.c
@@ -5,21 +5,21 @@
 #include "stm32f4xx_hal_tim.h"
 
 
-void Motor_back(){
+void Motor_Back(){
     HAL_GPIO_WritePin(Motor_Left_AIN1_GPIO_Port, Motor_Left_AIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Left_AIN2_GPIO_Port, Motor_Left_AIN2_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(Motor_Right_BIN1_GPIO_Port, Motor_Right_BIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Right_BIN2_GPIO_Port, Motor_Right_BIN2_Pin, GPIO_PIN_SET);
 }
 
-void Motor_forward(){
+void Motor_Forward(){
     HAL_GPIO_WritePin(Motor_Left_AIN1_GPIO_Port, Motor_Left_AIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Left_AIN2_GPIO_Port, Motor_Left_AIN2_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(Motor_Right_BIN1_GPIO_Port, Motor_Right_BIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Right_BIN2_GPIO_Port, Motor_Right_BIN2_Pin, GPIO_PIN_SET);
 }
 
-void Motor_turn_Left(int Steering){
+void Motor_Turn_Left(int Steering){
     HAL_GPIO_WritePin(Motor_Left_AIN1_GPIO_Port, Motor_Left_AIN1_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(Motor_Left_AIN2_GPIO_Port, Motor_Left_AIN2_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(Motor_Right_BIN1_GPIO_Port, Motor_Right_BIN1_Pin, GPIO_PIN_RESET);
@@ -27,7 +27,7 @@ void Motor_turn_Left(int Steering){
 
 }
 
-void Motor_turn_Right(int Steering){
+void Motor_Turn_Right(int Steering){
     HAL_GPIO_WritePin(Motor_Left_AIN1_GPIO_Port, Motor_Left_AIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Left_AIN2_GPIO_Port, Motor_Left_AIN2_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(Motor_Right_BIN1_GPIO_Port, Motor_Right_BIN1_Pin, GPIO_PIN_SET);
@@ -36,7 +36,7 @@ void Motor_turn_Right(int Steering){
 }
 
 
-void  Motor_stop(){
+void  Motor_Stop(){
     HAL_GPIO_WritePin(Motor_Left_AIN1_GPIO_Port, Motor_Left_AIN1_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Left_AIN2_GPIO_Port, Motor_Left_AIN2_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(Motor_Right_BIN1_GPIO_Port, Motor_Right_BIN1_Pin, GPIO_PIN_RESET);
--- Core/Hardware/Motor.h
+++ Core/Hardware/Motor.h
@@ -1,10 +1,10 @@
 #ifndef __MOTOR_H
 #define __MOTOR_H
-void Motor_turn_Left(int Steering Angle);
-void Motor_turn_Right(int Steering Angle);
-void Motor_back();
-void Motor_stop();
-void Motor_forward();
+void Motor_Turn_Left(int Steering Angle);
+void Motor_Turn_Right(int Steering Angle);
+void Motor_Back();
+void Motor_Stop();
+void Motor_Forward();
 
 
 
