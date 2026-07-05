8383971faff4071cabbf6b0c8c60610903ebad05
WeiZe-Lu <854316318@qq.com>
2026-07-05 11:45:48 +0800

    change oled position

--- Core/Hardware/MAX98357.c
+++ Core/Hardware/MAX98357.c
@@ -6,47 +6,50 @@
 #include "base64.h"
 
 
-#define MAX_SAMPLES 1500
-extern I2S_HandleTypeDef hi2s3;
-extern char * json_rx_buffer;
+#define MAX_SAMPLES 1280 / 2
+extern I2S_HandleTypeDef hi2s4;
+// extern char * json_rx_buffer;
 static uint32_t I2s_TX_Buffer[MAX_SAMPLES];
-static volatile uint32_t sample_count = 0;
+extern uint16_t i2s_rx_buffer[];
+// static volatile uint32_t sample_count = 1280;
 
-static uint32_t _get_str_len(const char *str) {
-    if (str == NULL) return 0;
-    uint32_t len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
-}
+// static uint32_t _get_str_len(const char *str) {
+//     if (str == NULL) return 0;
+//     uint32_t len = 0;
+//     while (str[len] != '\0') {
+//         len++;
+//     }
+//     return len;
+// }
 
 
-void Get_audio_data(){
+// void Get_audio_data(){
 
-    cJSON * root = cJSON_Parse(json_rx_buffer);
-    if (root != NULL){
-        static uint8_t base64_buffer[MAX_SAMPLES * 3];
-        int len = base64_decode_safe(root->valuestring, _get_str_len(root->valuestring), base64_buffer, sizeof(base64_buffer));
-        sample_count = (uint32_t)(len / 3);
-        if (sample_count > MAX_SAMPLES) sample_count = MAX_SAMPLES;
-        for (int i = 0;i < sample_count;i++){
-            int index = i* 3;
-            uint32_t raw_data = base64_buffer[index] << 16 | base64_buffer[index + 1] << 8 | base64_buffer[index + 2];
-            if (raw_data & 0x00800000){
-                raw_data |= 0xFF000000;
-            }
+//     cJSON * root = cJSON_Parse(json_rx_buffer);
+//     if (root != NULL){
+//         static uint8_t base64_buffer[MAX_SAMPLES * 3];
+//         int len = base64_decode_safe(root->valuestring, _get_str_len(root->valuestring), base64_buffer, sizeof(base64_buffer));
+//         sample_count = (uint32_t)(len / 3);
+//         if (sample_count > MAX_SAMPLES) sample_count = MAX_SAMPLES;
+//         for (int i = 0;i < sample_count;i++){
+//             int index = i* 3;
+//             uint32_t raw_data = base64_buffer[index] << 16 | base64_buffer[index + 1] << 8 | base64_buffer[index + 2];
+//             if (raw_data & 0x00800000){
+//                 raw_data |= 0xFF000000;
+//             }
             
-            I2s_TX_Buffer[i] = raw_data;
-        }
-        cJSON_Delete(root);
+//             I2s_TX_Buffer[i] = raw_data;
+//         }
+//         cJSON_Delete(root);
 
-    }
+//     }
     
    
-}
+// }
+
+
 
 
 void MAX98357_I2S_Send(){
-    HAL_I2S_Transmit_DMA(&hi2s3, (uint16_t*)I2s_TX_Buffer, sample_count);
+    HAL_I2S_Transmit_DMA(&hi2s4, (uint16_t*)i2s_rx_buffer, 1280 / 2);
 }
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -56,6 +56,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_spi3_rx;
+extern DMA_HandleTypeDef hdma_spi4_tx;
 extern DMA_HandleTypeDef hdma_spi2_rx;
 extern TIM_HandleTypeDef htim3;
 /* USER CODE BEGIN EV */
@@ -311,6 +312,20 @@ void EXTI15_10_IRQHandler(void)
   /* USER CODE BEGIN EXTI15_10_IRQn 1 */
 
   /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA2 stream1 global interrupt.
+  */
+void DMA2_Stream1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream1_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_spi4_tx);
+  /* USER CODE BEGIN DMA2_Stream1_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream1_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
